bd0462dea9afebe7b255d8880d44049cbe32ef63
Beyah96 <bahsaid96@gmail.com>
2024-09-01 13:24:47 +0200

    operators with c++

---
+++ section10_operators/section10_operators/section10_operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int A = 10, B = 20;
+
+	cout << "A + B = " << A + B << endl;
+	cout << "A - B = " << A - B << endl;
+	cout << "A * B = " << A * B << endl;
+	cout << "A / B = " << A / B << endl;
+	cout << "A % B = " << A % B << endl;
+
+	return 0;
+}
