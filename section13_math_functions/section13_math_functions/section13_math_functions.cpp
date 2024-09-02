eac7a2f6cfabde1af706935aef696891d28fcc3b
Beyah96 <bahsaid96@gmail.com>
2024-09-02 23:09:29 +0200

    round function

--- section13_math_functions/section13_math_functions/section13_math_functions.cpp
+++ section13_math_functions/section13_math_functions/section13_math_functions.cpp
@@ -6,9 +6,12 @@ using namespace std;
 int main() {
 
 	double x = 64;
+	cout << "Round value of 2.4 : " << round(2.4) << endl;
+	cout << "Round value of 2.5 : " << round(2.5) << endl;
+	cout << "Round value of 2.7 : " << round(2.7) << endl;
 
-	cout << "Square rout of 64 : " << sqrt(x) << endl;
 	cout << "Square rout of 50 : " << sqrt(50) << endl;
+	cout << "Round value of the quare rout of 50 : " << round(sqrt(50)) << endl;
 
 	return 0;
 }
