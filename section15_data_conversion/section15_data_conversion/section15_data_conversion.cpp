e87134cf5640acaaf098e4133456e49e15045e54
Beyah96 <bahsaid96@gmail.com>
2024-09-04 17:22:56 +0200

    using toString function

--- section15_data_conversion/section15_data_conversion/section15_data_conversion.cpp
+++ section15_data_conversion/section15_data_conversion/section15_data_conversion.cpp
@@ -5,18 +5,15 @@ using namespace std;
 
 int main() {
 
-	string str = "123.456";
+	int Num1 = 123;
+	double Num2 = 18.99;
 
-	int num_int = stoi(str);
+	string St1, St2;
 
-	float num_float = stof(str);
-
-	double num_double = stod(str);
-	
-
-	cout << "num_int: " << num_int << endl;
-	cout << "num_float: " << num_float << endl;
-	cout << "num_double: " << num_double << endl;
+	St1 = to_string(Num1);
+	St1 = to_string(Num2);
+	cout << St1 << endl;
+	cout << St2 << endl;
 
 	return 0;
 }
