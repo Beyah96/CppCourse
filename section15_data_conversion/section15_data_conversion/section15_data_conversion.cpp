9e54e193d26369513e8721ea1a269481f4be50ff
Beyah96 <bahsaid96@gmail.com>
2024-09-04 17:25:09 +0200

    data conversion

--- section15_data_conversion/section15_data_conversion/section15_data_conversion.cpp
+++ section15_data_conversion/section15_data_conversion/section15_data_conversion.cpp
@@ -4,6 +4,44 @@
 using namespace std;
 
 int main() {
+	int Num1;
+	double Num2 = 18.99;
+
+	Num1 = Num2;
+
+	Num1 = (int)Num2;
+
+	Num1 = int(Num2);
+
+
+	cout << Num1 << endl;
+
+
+	cout << "**************************************************\n";
+
+	string str = "123.456";
+
+	int num_int = stoi(str);
+
+	float num_float = stof(str);
+
+	double num_double = stod(str);
+
+	cout << "num_int: " << num_int << endl;
+	cout << "num_float: " << num_float << endl;
+	cout << "num_double: " << num_double << endl;
+
+	cout << "**************************************************\n";
+
+	string str = "123.456";
+
+	int num_int = stoi(str);
+
+	float num_fllot = stof(str);
+
+	double num_double = stod(str);
+
+	cout << "**************************************************\n";
 
 	int Num1 = 123;
 	double Num2 = 18.99;
