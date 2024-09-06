0256dc2ac9fb231e99866eb1a8aa07530668f55f
Beyah96 <bahsaid96@gmail.com>
2024-09-06 12:17:41 +0200

    function part1

---
+++ section18_function_part1/section18_function_part1/section18_function_part1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void myFunction() {
+
+	cout << "This is my first function, it got excuted :-) " << endl;
+
+}
+void mySecondFunction() {
+	cout << "This is my second function : -)" << endl;
+}
+
+int main()
+{
+
+	myFunction();
+
+	mySecondFunction();
+	return 0;
+}
