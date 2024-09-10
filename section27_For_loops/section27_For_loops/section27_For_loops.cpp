f8284bc46ac08345af969132b5d90aac6ec02770
Beyah96 <bahsaid96@gmail.com>
2024-09-10 23:29:41 +0200

    For loops

---
+++ section27_For_loops/section27_For_loops/section27_For_loops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+
+	for (int i = 0; i <= 5; i++) {
+		cout << i << endl;
+	}
+
+	for (int i = 15; i >= 1; i-=2) {
+		cout << i << endl;
+	}
+
+}
